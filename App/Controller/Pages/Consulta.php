25510bc96fe3b1c6cdb3b0acf45cec4d51d0e934
José Calumbo <josekalumbo@hotmail.com>
2025-10-16 00:28:27 +0100

    PK ESTA INDO
    VAMOS

--- App/Controller/Pages/Consulta.php
+++ App/Controller/Pages/Consulta.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Controller\Pages;
 
@@ -10,101 +10,111 @@ use \App\Model\Entity\VendedorDao;
 use \App\Utils\View;
 
 
-Class Consulta extends Page{
+class Consulta extends Page
+{
 
     // funcão para fazer pesquisa 
-    private static function getConsultasEspera($request,&$obPagination){
+    private static function getConsultasEspera($request, &$obPagination)
+    {
 
         $queryParam = $request->getQueryParams();
 
-        $obPagination = new Pagination(null,null,null);
-            
-         // Var que retorna o conteudo
-         $item='';
-         
-         $buscar = filter_input(INPUT_GET, 'pesquisar',FILTER_SANITIZE_STRING);
-             
-             $condicoes = [
-                 strlen($buscar) ? 'nome_paciente LIKE "%'.$buscar.'%"': null,
-            ];
-                
-            // coloca na consulta sql
-            $where = implode(' AND ',$condicoes);
-                
-            //quantidade total de registros da tabela user
-             $quantidadetotal = ConsultorioDao::listarTriagemFeita($where,'risco_triagem',null,'COUNT(*) as quantidade')->fetchObject()->quantidade;
+        $obPagination = new Pagination(null, null, null);
 
-             //pagina actual 
-              $queryParams = $request->getQueryParams();
-              $paginaAtual = $queryParams['page'] ?? 1;
+        // Var que retorna o conteudo
+        $item = '';
 
-              // instancia de paginacao
-              $obPagination = new Pagination($quantidadetotal,$paginaAtual,9);
+        $buscar = filter_input(INPUT_GET, 'pesquisar', FILTER_SANITIZE_STRING);
 
-             $resultado = ConsultorioDao::listarTriagemFeita($where,'zona',$obPagination->getLimit());
+        $condicoes = [
+            strlen($buscar) ? 'nome_paciente LIKE "%' . $buscar . '%"' : null,
+        ];
 
-             while($obPacientesEspera = $resultado->fetchObject(ConsultorioDao::class)){
+        // coloca na consulta sql
+        $where = implode(' AND ', $condicoes);
 
-                $item .= View::render('consulta/listarConsulta',[
-                    'id_zona'=>$obPacientesEspera->id_triagem,
-                    'zona'=>$obPacientesEspera->zona,
-                    'iniciovenda'=>$obPacientesEspera->inicio_venda,
-                    'fimvenda'=>$obPacientesEspera->fim_venda,
-                    'mercado'=>$obPacientesEspera->mercado
-                ]);
-            }
+        //quantidade total de registros da tabela user
+        $quantidadetotal = ConsultorioDao::listarTriagemFeita($where, 'risco_triagem', null, 'COUNT(*) as quantidade')->fetchObject()->quantidade;
 
-            $queryParam = $request->getQueryParams();
+        //pagina actual 
+        $queryParams = $request->getQueryParams();
+        $paginaAtual = $queryParams['page'] ?? 1;
 
-            if($queryParam['pesquisar'] ?? '') {
-                    
-                    return View::render('pesquisar/item',[
-                        'pesquisa'=>$buscar,
-                        'resultados'=>$item,
-                        'numResultado'=>$quantidadetotal,
-                    ]);
-            }
+        // instancia de paginacao
+        $obPagination = new Pagination($quantidadetotal, $paginaAtual, 6);
+
+        $resultado = ConsultorioDao::listarTriagemFeita($where, 'risco_triagem', $obPagination->getLimit());
+
+        while ($obPacientesEspera = $resultado->fetchObject(ConsultorioDao::class)) {
+
+            // formata a idade do paciente
+            $formataIdade = date("Y", strtotime($obPacientesEspera->nascimento_paciente));
+            $idade = date("Y") - $formataIdade;
+
+            $item .= View::render('consulta/listarConsulta', [
+                'id_paciente' => $obPacientesEspera->id_triagem,
+                'imagem' => $obPacientesEspera->imagem_paciente,
+                'nome_paciente' => $obPacientesEspera->nome_paciente,
+                'genero' => $obPacientesEspera->genero_paciente,
+                'idade' => $idade,
+                'perioridade' => $obPacientesEspera->risco_triagem
+            ]);
+        }
+
+        $queryParam = $request->getQueryParams();
+
+        if ($queryParam['pesquisar'] ?? '') {
+
+            return View::render('pesquisar/item', [
+                'pesquisa' => $buscar,
+                'resultados' => $item,
+                'numResultado' => $quantidadetotal,
+            ]);
+        }
 
         return $item;
     }
 
     // Metodo para apresentar a tela Consulta 
-    public static function telaConsulta($request){
+    public static function telaConsulta($request)
+    {
 
-        $buscar = filter_input(INPUT_GET, 'pesquisar',FILTER_SANITIZE_STRING);
-        $content = View::render('consulta/Consultorio',[
-             'pesquisar'=>$buscar,
-            'listarConsulta'=>self::getConsultasEspera($request,$obPagination),
-            'paginacao'=>parent::getPaginacao($request,$obPagination)
+        $buscar = filter_input(INPUT_GET, 'pesquisar', FILTER_SANITIZE_STRING);
+        $content = View::render('consulta/Consultorio', [
+            'pesquisar' => $buscar,
+            'listarConsulta' => self::getConsultasEspera($request, $obPagination),
+            'paginacao' => parent::getPaginacao($request, $obPagination)
         ]);
         return parent::getPage('Painel Consulta', $content);
     }
 
     //metodo responsavel para pegar os paciente
-    public static function getPaciente(){
-            $paciente = '';
-            $paciente = VendedorDao::listarVendedor();
-            while( $obpaciente = $paciente->fetchObject(VendedorDao::class)){
-                $paciente .= View::render('item/paciente', [
-                    'nome'=>$obpaciente->nome,
-                    'value'=>$obpaciente->id
-                ]);
-            }
-            return $paciente;
+    public static function getPaciente()
+    {
+        $paciente = '';
+        $paciente = VendedorDao::listarVendedor();
+        while ($obpaciente = $paciente->fetchObject(VendedorDao::class)) {
+            $paciente .= View::render('item/paciente', [
+                'nome' => $obpaciente->nome,
+                'value' => $obpaciente->id
+            ]);
+        }
+        return $paciente;
     }
 
     // Metodo que cadastra consultas
-    public static function cadastrarConsulta($request){
+    public static function cadastrarConsulta($request)
+    {
 
         $obZona = new ZonaDao;
 
-        if (isset($_POST['a'], $_POST['a'], $_POST['a'], $_POST['a'])){
-            
+        if (isset($_POST['a'], $_POST['a'], $_POST['a'], $_POST['a'])) {
+
             $obZona->zona = $_POST['zona'];
             $obZona->inicio_venda = $_POST['inicio'];
             $obZona->fim_venda = $_POST['fim'];
             $obZona->mercado = $_POST['mercado'];
-           // $obZona->cadastrarZona();
+            // $obZona->cadastrarZona();
             // Redireciona para Painel Zona 
             $request->getRouter()->redirect('/zona?msg=cadastrado');
         }
@@ -113,35 +123,37 @@ Class Consulta extends Page{
             'titulo' => 'RegiConsultas',
             'nome' => 'Ana luis',
             'button' => 'Salvar',
-            'zona'=>'',
-            'fim'=>'',
-            'inicio'=>'',
+            'zona' => '',
+            'fim' => '',
+            'inicio' => '',
             //'paciente'=>self::getPaciente(),
-            'mercado'=>''
+            'mercado' => ''
 
         ]);
         return parent::getPage('Cadastrar nova Zona', $content);
     }
 
     // Metodo para apresentar a tela Consulta 
-    public static function comfirmarConsulta($request){
+    public static function comfirmarConsulta($request)
+    {
 
-        $buscar = filter_input(INPUT_GET, 'pesquisar',FILTER_SANITIZE_STRING);
-        $content = View::render('consulta/formConfirmaConsulta',[
-             'pesquisar'=>$buscar,
-             'listarZona'=>self::getConsultasEspera($request,$obPagination),
-             'paginacao'=>parent::getPaginacao($request,$obPagination)
+        $buscar = filter_input(INPUT_GET, 'pesquisar', FILTER_SANITIZE_STRING);
+        $content = View::render('consulta/formConfirmaConsulta', [
+            'pesquisar' => $buscar,
+            'listarZona' => self::getConsultasEspera($request, $obPagination),
+            'paginacao' => parent::getPaginacao($request, $obPagination)
         ]);
         return parent::getPage('Painel Consulta', $content);
     }
 
     // Metodo que cadastra consultas
-    public static function cadastrarConsulta1($request){
+    public static function cadastrarConsulta1($request)
+    {
 
         $obZona = new ZonaDao;
 
-        if (isset($_POST['zona'], $_POST['inicio'], $_POST['fim'], $_POST['mercado'])){
-            
+        if (isset($_POST['zona'], $_POST['inicio'], $_POST['fim'], $_POST['mercado'])) {
+
             $obZona->zona = $_POST['zona'];
             $obZona->inicio_venda = $_POST['inicio'];
             $obZona->fim_venda = $_POST['fim'];
@@ -154,66 +166,67 @@ Class Consulta extends Page{
         $content = View::render('consulta/formConsulta', [
             'titulo' => 'Registrar Nova Consultas',
             'button' => 'Salvar',
-            'zona'=>'',
-            'fim'=>'',
-            'inicio'=>'',
+            'zona' => '',
+            'fim' => '',
+            'inicio' => '',
             //'paciente'=>self::getPaciente(),
-            'mercado'=>''
+            'mercado' => ''
 
         ]);
         return parent::getPage('Cadastrar nova Zona', $content);
     }
 
     // Metodo que Edita Zona
-    public static function editarZona($request,$id_zona){
+    public static function editarZona($request, $id_zona)
+    {
 
         $obZona = ZonaDao::getZona($id_zona);
 
-        if (isset($_POST['a'], $_POST['a'], $_POST['a'], $_POST['a'])){
-                
+        if (isset($_POST['a'], $_POST['a'], $_POST['a'], $_POST['a'])) {
+
             $obZona->zona = $_POST['zona'];
             $obZona->inicio_venda = $_POST['inicio'];
             $obZona->fim_venda = $_POST['fim'];
             $obZona->mercado = $_POST['mercado'];
-           // $obZona->AtualizarZona();
-    
+            // $obZona->AtualizarZona();
+
             // Redireciona para Painel Zona 
             $request->getRouter()->redirect('/zona?msg=editado');
         }
 
         $content = View::render('zonas/formZona', [
-                'titulo' => 'Editar Zona',
-                'nome' => 'Elizandra Bianca Pedro',
-                'button' => 'Editar',
-                'zona'=> $obZona->zona,
-                'fim'=> $obZona->fim_venda,
-                'inicio'=> $obZona->inicio_venda,
-                'mercado'=> $obZona->mercado
+            'titulo' => 'Editar Zona',
+            'nome' => 'Elizandra Bianca Pedro',
+            'button' => 'Editar',
+            'zona' => $obZona->zona,
+            'fim' => $obZona->fim_venda,
+            'inicio' => $obZona->inicio_venda,
+            'mercado' => $obZona->mercado
         ]);
-         return parent::getPage('Atualizar Zona', $content);
+        return parent::getPage('Atualizar Zona', $content);
     }
 
     // Metodo que apagar Zona
-    public static function apagarZona ($request,$id_zona){
+    public static function apagarZona($request, $id_zona)
+    {
 
         $obZona = ZonaDao::getZona($id_zona);
 
-        if (isset($_POST['apagar'])){
+        if (isset($_POST['apagar'])) {
 
             $obZona->apagarZona();
-            
+
             // Redireciona para Painel Zona 
             $request->getRouter()->redirect('/zona?msg=apagado');
         }
 
         $content = View::render('zonas/deletaZona', [
-                'titulo' => 'Apagar Zona',
-                'button' => 'Sim',
-                'zona'=> $obZona->zona,
-                'id_zona'=> $obZona->id_zona,
-                'mercado'=> $obZona->mercado
+            'titulo' => 'Apagar Zona',
+            'button' => 'Sim',
+            'zona' => $obZona->zona,
+            'id_zona' => $obZona->id_zona,
+            'mercado' => $obZona->mercado
         ]);
-         return parent::getPage('Apagar Zona', $content);
+        return parent::getPage('Apagar Zona', $content);
     }
-
 }
